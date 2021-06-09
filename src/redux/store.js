577a465290f6c4ad9843650bbcba91ff4e602fc3
BrownEdgar <nikogosjanedgar@gmail.com>
2021-06-09 13:44:39 +0400

    createStore with combineReducers

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,53 @@
-import { createStore } from "redux";
+import { combineReducers, createStore } from "redux";
 
-const store = createStore(function (state, action) {
-	if (action.type === "change-friend-name") {
-		return { friend: { name: action.payload.name} }
-	}
-	return state;
 
-}, {
+const initialState = {
+	serials: [
+		{
+			id: 1,
+			title: "friends",
+			ratings: "9.8",
+			actors: ['Courteney Cox', 'Jennifer Aniston', 'Lisa Kudrow', 'Matt LeBlanc', 'Matthew Perry', 'David Schwimmer'],
+			poster: "https://wallpapercave.com/wp/wp3256631.jpg"
+		},
+		{
+			id: 2,
+			title: "Prison Break",
+			ratings: "9.7",
+			actors: ['Wentworth Miller', 'Dominic Purcell', 'Robert Knepper', 'Sarah Wayne Callies', 'William Fichtner'],
+			poster: "https://wallpapercave.com/wp/wp3988846.jpg"
+		}
+	],
+
 	friend: {
 		name: "Jhon snow"
 	}
-})
+}
+
+function friendReduser(state={}, action) {
+	if (action.type === "change-friend-name") {
+		return { 
+			...state,
+			 name: action.payload.name
+		 }
+	}
+	return state;
+}
+function serialsReduser(state = [], action) {
+	if (action.type === "add-serials") {
+		return {
+			...state,
+			
+		}
+	}
+	return state;
+}
+
+
+const store = createStore(combineReducers({
+	friend:friendReduser,
+	serials:serialsReduser,
+}), initialState)
+
+
 export default store
