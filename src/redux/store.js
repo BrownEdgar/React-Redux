ac8d7d08202f668836ac6cb9f25b79ab9b7e7b64
BrownEdgar <nikogosjanedgar@gmail.com>
2022-01-13 23:39:13 +0400

    fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,32 @@
 //////////////////////////////////////////////////////////////
-//						  ԱՅՍ ՄԱՍՈՒՄ (3)					
-//  friendReduser & serialsReduser ֆունկցիաները և State-ի համապատասղան մասերը արդեն առանձնացվում են առանձին ֆայլերում, այստեղ նրանք օգտագործվում են import-ի միջոցով
-// friendReduser-ում ավելացված է "add-friends" action-ը
-//
-
+//						  ԱՅՍ ՄԱՍՈՒՄ (4)
+// * Այս մասում ստեղծվում է "myMiddleWare" անունով ֆունկցիան, այն կաշխատի ամեն մի փոփոխության ժամանակ
+// * "createStore"-ի մեջ փոխանցվում է  "applyMiddleware"-ին  ("redux"-ում գտնվող ֆունկցիային) որպես 3-րդ արգումենտ  տող 35
+// * Middleware-րը կարող են լինալ բազմաթիվ
+// * Middleware-րը կարող են միջանկյալ փոխել store-ը (մեր տվյալները) քանի,որ նրանց է հասնում "action"-ը
 //////////////////////////////////////////////////////////////
 
+import { applyMiddleware } from "redux";
 import { combineReducers, createStore } from "redux";
 import { friendReduser, initialFriendsValue } from './features/friends'
 import { serialsReduser, initialSerialsValue } from './features/seriales'
 
+// mas 4 middleWare
+let count = 0
+function myMiddleWare(store) {
+	return function (next) {
+		return function (action) {
+			console.log(`middleWare run ${count++} times`);
+			//Այս տօղը եթե չգրեն առաջ չի անցնի, կտպի միայն "log"-ը  
+			return next(action)
+		}
+	}
+}
+
+const middleNameChanger = (store) => (next) => (action) => {
+	action.payload.name += `🎁`;
+	return next(action);
+}
 
 const initialState = {
 	serials: initialSerialsValue,
@@ -20,7 +37,9 @@ const initialState = {
 const store = createStore(combineReducers({
 	friend:friendReduser,
 	serials:serialsReduser,
-}), initialState)
+}), 
+	initialState,
+	applyMiddleware(myMiddleWare, middleNameChanger))
 
 
 export default store
