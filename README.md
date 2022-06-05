abb5d47885924ddcf1bb910d393509bb1e1c049d
BrownEdgar <nikogosjanedgar@gmail.com>
2022-06-05 14:55:06 +0400

    add some explanation for combineReducers

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Getting Started with Create React App
+
+`combineReducers` - Это метод, который позволяет вместо того, чтобы создавать один огромный reducer для всего состояния приложения сразу, разбивать его на отдельные модули.
+փոխանցվում է createStore ֆունկցիային, որպես առաջին արգումենտ,
+
+- Այն ստանում է մեկ արգումենտ օրինակ `{name: nameReducer}`, որը նշանակում է որ "state"-ում գտնվող name-ի պատասխանատուն լինելու է "nameReducer" ֆունկցիան։
+
+#
+
+    const store = createStore(combineReducers({
+    friend:friendReduser,
+    serials:serialsReduser,
+    }), initialState)
--- src/Friends.js
+++ src/Friends.js
@@ -1,22 +1,21 @@
 
-import { useSelector, useDispatch } from "react-redux"
+import { useSelector } from "react-redux"
 
 function Friends() {
-//	Էստեղ ամբողջ "state"-ից վերցնում ենք միայն այն ինչ պետք է կոնկրետ այս կոմպոնենտին
-// վերցնում ենք միայն "friends" զանգվածը
-	const serials = useSelector(state => state.serials)
-	const dispatch = useDispatch()
+  //	Էստեղ ամբողջ "state"-ից վերցնում ենք միայն այն ինչ պետք է կոնկրետ այս կոմպոնենտին
+  // վերցնում ենք միայն "friends" զանգվածը
+  const serials = useSelector(state => state.serials)
 
-	return (
-		<div className="App">
-			<h1>friends</h1>
-			<ul>
-				{serials.map((elem, index) => {
-				return <li key={index}>{JSON.stringify(elem,null,1)}</li>
-			})}
-			</ul>	
-		</div>
-	);
+  return (
+    <div className="App">
+      <h1>friends</h1>
+      <ul>
+        {serials.map((elem, index) => {
+          return <li key={index}>{JSON.stringify(elem, null, 1)}</li>
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default Friends;
