6da6c1777828d11616e31e63a167ec48cff494b6
BrownEdgar <nikogosjanedgar@gmail.com>
2022-06-23 18:39:20 +0400

    preparatory works

--- src/App.js
+++ src/App.js
@@ -3,23 +3,11 @@ import { useSelector, useDispatch } from "react-redux"
 
 
 function App() {
-	const todoList = useSelector(state => state.todo);
+	const state = useSelector(state => state);
 	const dispatch = useDispatch();
 	return (
 		<div className="App">
-			<pre>
-				{JSON.stringify(todoList,null,1)}
-			
-			</pre>
-			<button onClick={() => dispatch({
-				type: "ADD-TODO",
-				payload: {
-					"userId": 1,
-					"id": 5,
-					"title": "My todo",
-					"completed": false
-				}
-			})}>add todo</button>
+
 		</div>
 	);
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,26 +1,7 @@
 import { createStore } from 'redux'
 
 const initialState = {
-	todo:[
-		{
-			"userId": 1,
-			"id": 1,
-			"title": "delectus aut autem",
-			"completed": false
-		},
-		{
-			"userId": 1,
-			"id": 2,
-			"title": "quis ut nam facilis et officia qui",
-			"completed": false
-		},
-		{
-			"userId": 1,
-			"id": 3,
-			"title": "fugiat veniam minus",
-			"completed": false
-		}
-	]
+
 }
 
 const isConteins = (list,newItem) => { 
