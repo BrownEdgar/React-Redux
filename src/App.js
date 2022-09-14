cbc6fced760b298d467f590812afb328c672c90e
BrownEdgar <nikogosjanedgar@gmail.com>
2022-09-14 17:46:26 +0400

    add 'mapStateToProps' example & comment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useSelector, useDispatch } from "react-redux"
+import { useDispatch } from "react-redux"
 import { incrementBy } from './features/product/prodactSlice';
 import { Product } from './features/product/Product';
 
--- src/features/product/Product.js
+++ src/features/product/Product.js
@@ -1,4 +1,16 @@
+////////////////////////////////////////////////////////////////////////
+// link => https://react-redux.js.org/using-react-redux/connect-mapstate
+//  connect(mapStateToProps) ևս մեկ հնարավորություն է "state"-ից տվյալներ վերցնելու
+// mapStateToProps-ը ստանում 2 պարամետր, և պետք է վերադարձնի օբյեկտ
+// 1․ "state" Հիմնական "state"-ն է։
+// 2․ "ownProps" Կոմպոնենտին փոխանցած ՛props՛-ն են առանձին օբյեկտի տեսքով
+// mapStateToProps-ի միջոցով ՛props՛-ն ստուգելով կարող ենք կոմպոնենտին տալ համապատասղան կտորը,
+// կամ եթե "state"-ից տվյալներ վերցնելու համար պետք են կոմպոնենտի ՛props՛-րը
+// Կանչվում է ամեն անգամ երբ "state"-ը փոխվում է 
+////////////////////////////////////////////////////////////////////////
+
 import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
 import { useSelector, useDispatch } from 'react-redux'
 import { getAll, addProductsAsync } from './prodactSlice'
 
@@ -13,7 +25,6 @@ export function Product() {
     dispatch(addProductsAsync('https://jsonplaceholder.typicode.com/todos?_limit=3'))
   }, [])
 
-
   return (
     <div>
       <div>
@@ -22,3 +33,15 @@ export function Product() {
     </div>
   )
 }
+
+
+const mapStateToProps = (state, ownProps) => {
+  const { products } = state
+  return {
+    products,
+    // ՛ownProps՛-ը այստեղկարող ենք չվերադարձնել, քանի որ ՛connect()՛-ը ավտոմատ կմիավորի այս թ տարբեր ՛props՛-ը մեկում!
+    ownProps
+  }
+}
+
+export default connect(mapStateToProps)(Product)
